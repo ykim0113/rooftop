b975fe1510e967c42a671617a3a236503eb6c139
ykim0113 <ykim0113@gmail.com>
2018-02-02 19:37:25 -0500

    rooftop commit

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ Array.prototype.pick = function() {
 }
 
 
-//get flickr photo under rooftop
+/**
+* Generate Flickr photo based on cityname query
+**/
 function generate(query)
     {
 
@@ -27,12 +29,6 @@ function generate(query)
         query = cityName;
         $.ajax({
             url: 'https://api.flickr.com/services/rest/?method=flickr.photos.search&text='+ cityName + ' rooftop&format=json&nojsoncallback=1&api_key=' + FlickrAPIKey, success: function(data) {
-
-
-
-           //original
-          //'https://api.flickr.com/services/rest/?method=flickr.photos.search&text='+ cityName +' tour&format=json&nojsoncallback=1&api_key=' + FlickrAPIKey
-
 
             //data = photo
             console.log("   got images");
@@ -57,9 +53,10 @@ function generate(query)
     var geocoder;
     var map;
 
-
-
-    // function to geocode an address and plot it on a map
+    /**
+    * Gets geographical location of an address of the city
+    * Plots the city on Google Map
+    */
     function codeAddress(address) {
         var address = query;
         geocoder.geocode( {'address': address}, function(results, status) {
@@ -70,48 +67,56 @@ function generate(query)
                 // latitude = 33.748995;
                 // longitude = -84.387982;
 
-                getWeather(cityName);                 // get weather for returned lat/long
-                map.setCenter(results[0].geometry.location);                // center the map on address
-                var marker = new google.maps.Marker({                       // place a marker on the map at the address
+                // get weather for returned lat/long
+                getWeather(cityName);
+                // center the map on address
+                map.setCenter(results[0].geometry.location);
+                // place a marker on the map at the address
+                var marker = new google.maps.Marker({
                     map: map,
                     position: results[0].geometry.location
                 });
 
             } else {
-                alert('Geocode was not successful for the following reason: ' + status);
+                alert('Geocode was not successful for the following reason: '
+                 + status);
             }
         });
     }
 
-
-
-
-    // google.maps.event.addDomListener(window, 'load', initialize);       // setup initial map
-
+    /**
+    * Initialize Google Map
+    **/
     function initialize() {
-        geocoder        = new google.maps.Geocoder();                       // create geocoder object
-        var latlng      = new google.maps.LatLng(40.6700, -73.9400);        // set default lat/long (new york city)
-        var mapOptions  = {                                                 // options for map
+        geocoder        = new google.maps.Geocoder();
+        // set default lat/long (NYC)
+        var latlng      = new google.maps.LatLng(40.6700, -73.9400);
+        var mapOptions  = {
             zoom: 8,
             center: latlng
         }
-        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);   // create new map in the map-canvas div
+        // create new map in the map-canvas div
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     }
 
 
 
-    // function to get weather for an address
+    /**
+    * Function to get weather for an address
+    */
     function getWeather(address) {
         var address = cityName;
         if(cityName != null || !cityName.equals("")) {
             $("#weather").val("Retrieving weather...");
-            $.getJSON( "http://api.openweathermap.org/data/2.5/weather?q="+ cityName +'&appid=' + OpenWeatherAPIKey, function(data) {
-                var currWeather = new Array();                              // create array to hold our weather response data
-                currWeather['currTemp'] = Math.round(data.main.temp);               // current temperature
-
-                currWeather['description'] = data.weather[0].description;              // short text description (ie. rain, sunny, etc.)                currWeather['icon'] = "http://openweathermap.org/img/w/"+data.weather[0].icon+".png";   // 50x50 pixel png icon
-
-
+            $.getJSON( "http://api.openweathermap.org/data/2.5/weather?q="+
+                cityName +'&appid=' + OpenWeatherAPIKey, function(data) {
+                // create array to hold our weather response data
+                var currWeather = new Array();
+                // current temperature
+                currWeather['currTemp'] = Math.round(data.main.temp);
+                // short text description (ie. rain, sunny, etc.)
+                currWeather['description'] = data.weather[0].description;
+                // current temperature converetd to Celcius
                 var temp = currWeather['currTemp'] - 273.5;
 
                 var response = temp +"\xB0 and "+currWeather['description'];
@@ -121,7 +126,7 @@ function generate(query)
 
                     var latitude = data.coord.lat;
                     var longitude = data.coord.lon;
-                                          // create geocoder object
+
                     var latlng      = new google.maps.LatLng(latitude, longitude);        // set default lat/long (new york city)
                     var mapOptions  = {                                                 // options for map
                         zoom: 8,
